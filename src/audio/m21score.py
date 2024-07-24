81b96e856f9c45a2067088fe9dd40a7c594ed491
Darin <darinchauyf@gmail.com>
2024-07-24 13:15:50 +0800

    Added tree method for m21score

--- src/audio/m21score.py
+++ src/audio/m21score.py
@@ -1,5 +1,8 @@
 # Provides a wrapper for music21's Score class with some convenience functions
 
+from __future__ import annotations
+from fractions import Fraction
+from dataclasses import dataclass
 import music21 as m21
 import copy
 from music21.midi.translate import streamToMidiFile
@@ -7,18 +10,55 @@ import tempfile
 from src.util import is_ipython
 import base64
 from music21 import common
-from music21.stream.base import Stream
+from music21.stream.base import Stream, Score, Part, Opus
 import subprocess
 from .audio import Audio
+import warnings
+
+class M21NoteTree:
+    def __init__(self, start: Fraction, end: Fraction, obj: m21.base.Music21Object, children: list[M21NoteTree] | None = None):
+        self.start = start
+        self.end = end
+        self.obj = obj
+        self.children = children
+
+    def make_repr(self, indent: int = 4):
+        def make_repr_recursive(elem: M21NoteTree, current_indent: int):
+            indent_str = " " * current_indent
+            s = f"{indent_str}({elem.start} - {elem.end}) {elem.obj}"
+            if elem.children is None:
+                return s
+
+            strs = [s]
+            for c in elem.children:
+                strs.append(make_repr_recursive(c, current_indent=current_indent+indent))
+            return "\n".join(strs)
+        return make_repr_recursive(self, 0)
+
+    def __repr__(self):
+        return self.make_repr(indent = 4)
 
 class M21Score:
-    def __init__(self, score: Stream):
+    def __init__(self, score: Score):
+        if len(score.parts) != 2:
+            warnings.warn(f"Expected 2 parts in a score but found {len(score.parts)}. Not a big deal but might break future code.")
+        # TODO investigate and add support for parts / opus
         self._data = score
 
     @classmethod
     def parse(cls, path: str):
         """Read a music21 Stream object from an XML file or a MIDI file."""
-        return cls(m21.converter.parse(path))
+        # Purely for convenience
+        test_cases = {
+            "-test1": "resources/scores/Prelude in C Major.mid",
+            "-test2": "resources/scores/Musical Offering BWV 1079.mxl"
+        }
+        if path in test_cases:
+            path = test_cases[path]
+        score = m21.converter.parse(path)
+        if not isinstance(score, Score):
+            raise ValueError(f"The file {path} is parsed as a {score.__class__.__name__} which is not supported yet.")
+        return cls(score)
 
     def write_to_midi(self, path: str):
         """Write a music21 Stream object to a MIDI file."""
@@ -58,6 +98,9 @@ class M21Score:
     def show(self, fmt = None):
         """Calls the show method of the music21 Stream object. Refer to the music21 documentation for more information."""
         return self._data.show(fmt)
+
+    def make_tree(self):
+        return make_tree(self._data)
 
 def play_binary_midi_m21(b: bytes):
     """Play a midi file in bytes inside Jupyter"""
@@ -103,3 +146,46 @@ def convert_midi_to_wav(input_path: str, output_path: str, soundfont_path="~/.fl
     subprocess.call(['fluidsynth', '-ni', soundfont_path, input_path, '-F', output_path, '-r', str(sample_rate)],
         stdout=subprocess.DEVNULL if not verbose else None,
         stderr=subprocess.DEVNULL if not verbose else None)
+
+def float_to_fraction_time(f: float | Fraction, *, limit_denom: int = m21.defaults.limitOffsetDenominator) -> Fraction:
+    """Turn a float into a fraction
+    limit_denom (int): Limits the denominator to be less than or equal to limit_denom
+
+    Code referenced from music21.common.numberTools"""
+    if not isinstance(f, Fraction):
+        quotient, remainder = divmod(float(f), 1.)
+        remainder = Fraction(remainder).limit_denominator(limit_denom)
+        if quotient < -1:
+            quotient += 1
+            remainder = 1 - remainder
+        elif quotient == -1:
+            quotient = 0.0
+            remainder = remainder - 1
+    else:
+        quotient = int(float(f))
+        remainder = f - quotient
+        if quotient < 0:
+            remainder *= -1
+
+    return int(quotient) + remainder
+
+def _build_tree_recursive(stream: Stream) -> list[M21NoteTree]:
+    def pack_elem(in_element):
+        start = in_element.getOffsetBySite(stream)
+        start_timestamp = float_to_fraction_time(start)
+        end = start + in_element.duration.quarterLength
+        end_timestamp = float_to_fraction_time(end)
+        return start_timestamp, end_timestamp
+
+    children: list[M21NoteTree] = []
+    for element in stream:
+        start_timestamp, end_timestamp = pack_elem(element)
+        if isinstance(element, Stream):
+            c = _build_tree_recursive(element)
+            children.append(M21NoteTree(start_timestamp, end_timestamp, element, children=c))
+        else:
+            children.append(M21NoteTree(start_timestamp, end_timestamp, element))
+    return children
+
+def make_tree(stream: Stream):
+    return M21NoteTree(Fraction(), float_to_fraction_time(stream.duration.quarterLength), stream, _build_tree_recursive(stream))
